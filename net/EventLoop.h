0c0add8cb4a45627fa37a4dee5d3ee4f3e88f3bc
hijkzzz <janhu9527@gmail.com>
2016-12-15 22:57:07 +0800

    update

---
+++ net/EventLoop.h
@@ -0,0 +1,29 @@
+#ifndef NET_EVENTLOOP_H
+#define NET_EVENTLOOP_H
+
+#include <boost/noncopyable.hpp>
+
+class EventLoop : private boost::noncopyable {
+public:
+    EventLoop();
+    ~EventLoop();
+
+    void loop();
+
+    void assertInLoopThread() {
+        if (!isInLoopThread()) {
+            abortNotInLoopThread();
+        }
+    }
+
+    bool isInLoopThread() const { return threadId_ == CurrentThread::tid(); }
+
+private:
+    void abortNotInLoopThread();
+
+    bool        looping_;
+    const pid_t threadId_;
+};
+
+
+#endif //NET_EVENTLOOP_H
